49374ee664eafffea3affe2ed0d5c9a199871f92
viniciuslrangel <vinicius.l.rangel@gmail.com>
2016-03-13 11:34:23 -0300

    Project setup

---
+++ Source/WhereAmI/WhereAmIGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "WhereAmI.h"
+#include "WhereAmIGameMode.h"
+
+
+
+
---
+++ Source/WhereAmI/WhereAmIGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "WhereAmIGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WHEREAMI_API AWhereAmIGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
